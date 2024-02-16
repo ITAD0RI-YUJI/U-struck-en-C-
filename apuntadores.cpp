38e86858e461f138c88af3ae490ae489e3851a8f
Aizawa Tomo <diazpenajuansebastian24@gmail.com>
2024-02-15 22:05:43 -0500

    Agregando proceso para calcular el promedio del estudiante

--- apuntadores.cpp
+++ apuntadores.cpp
@@ -8,6 +8,7 @@ struct Estudiante {
     long long int codigo;
     string nombre;
     float notas [3]; 
+	float promedio = 0;
 };
 
 int main()
@@ -31,36 +32,44 @@ int main()
 	}
 	while (n<3); 
 	
-	Estudiante Alumno [n]; 
+	Estudiante Alumno[n]; 
     Estudiante* ap = Alumno; 
     
 	for (int i=0; i<n; i++)
 	{
 		cout<<"\nIngrese los datos del estudiante "<<i+1<<endl; 
 		cout<<"Ingrese su c�digo"<<endl; 
-		cin>>(ap+i)->codigo; 
+		cin>>(ap+i) -> codigo; 
 		cin.ignore();
 		
 		cout<<"\nIngrese su nombre"<<endl; 
-		getline(cin, (ap+i)->nombre);
+		getline(cin, (ap+i) -> nombre);
 		
 		for (int s=0; s<3; s++)
 		{
 			cout<<"\nIngrese las tres notas"<<endl; 
-			cin>> (ap+i)->notas [s]; 
+			cin>> (ap+i) -> notas[s]; 
 		}
 	}    
 	
 	for (int i=0; i<n; i++)
 	{
+		int suma = 0;
+
 		cout<<"\nDatos del estudiante "<< i+1<<endl;
-		cout<<"Nombre: " << (ap+i)->nombre<<endl; 
-		cout<<"C�digo: " << (ap+i)->codigo<<endl; 
+		cout<<"Nombre: " << (ap+i) -> nombre<<endl; 
+		cout<<"C�digo: " << (ap+i) -> codigo<<endl; 
 		
 		for (int s=0; s<3; s++)
 		{
-			cout<<"Nota :" <<(ap+i)->notas [s]<<endl; 
+			cout<<"Nota :" <<(ap+i) -> notas[s]<<endl; 
+
+			suma += (ap+i) -> notas[s];
 		}
+
+		(ap + i) -> promedio = suma / 3.0f; // Calcula el promedio
+
+		cout<<"Promedio: " << (ap+i) -> promedio<<endl; 
 	}
 	
 return 0; 
